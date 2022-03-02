f18cf5de4edc1bbd7de27d1dcbdedfcf67c6ee2d
error2215 <slyvkamm@gmail.com>
2022-03-02 18:03:59 +0200

    fix

--- coreminer.rb
+++ coreminer.rb
@@ -3,7 +3,8 @@ require_relative "lib/custom_download_strategy"
 class Coreminer< Formula
   desc "Official Coreminer implementation"
   homepage "https://coreblockchain.cc/"
-  url "https://github.com/catchthatrabbit/coreminer/v0.19.29.tar.gz", :using => CustomGitHubPrivateRepositoryDownloadStrategy
+  head "https://github.com/catchthatrabbit/coreminer", :using => :git
+#   url "https://github.com/catchthatrabbit/coreminer/v0.19.29.tar.gz", :using => CustomGitHubPrivateRepositoryDownloadStrategy
   version "0.19.29"
   sha256 "72beea23bf7f594eb4e4661ee34bb5c192ac13d4117fac4d92fddd02cce2b572"
   license "LGPL-3.0"
